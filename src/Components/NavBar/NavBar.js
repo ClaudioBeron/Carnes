c8e5da60559bb421c6374528172436934483a933
Claudio Beron <claudioberon@live.com.ar>
2022-03-28 23:11:10 -0300

    cree el componente NavBar y su respectivo archivo css

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+    return (<nav className="navegacion">
+
+        
+        <button> Empresa </button>
+        <button className="product"> Productos </button> 
+             <li> Vaca </li>
+             <li> Pollo </li>
+             <li> Cerdo </li>
+        <button> Contactos </button>
+         
+    </nav>
+
+    )
+}
+
+
+export default NavBar
