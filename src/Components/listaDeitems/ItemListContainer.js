de7e8fd834a452ea592fbbdf8c75edc4c7509ccf
Claudio Beron <claudioberon@live.com.ar>
2022-04-06 22:45:48 -0300

    cree carpetas item list, item  , con sus archivos js y un archivo mas de asyncmock.js

--- src/Components/listaDeitems/ItemListContainer.js
+++ src/Components/listaDeitems/ItemListContainer.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react"
+import { getProducts } from "../../asyncmock"
+import ItemList from "../ItemList/ItemList"
+
+
+
 const ItemListContainer = (props) =>{
+    const [products , setProducts] = useState ([])
+
+
+    useEffect(()=>{
+        getProducts().then(prod =>{
+            setProducts(prod)
+
+        })
+
+    },[])
     console.log(props.greeting)
     return(
+        <div>
         <h1>{props.greeting}</h1>
+        <ItemList products ={products}/>
+        </div>
 
     )
 }
---
+++ src/asyncmock.js
@@ -0,0 +1,13 @@
+const products =[
+    {id: 1, name: "vaca" , price: 1000, category: "carne" , stock :100 , description :"nalga" , img:"https://ar.all.biz/img/ar/catalog/75324.png"},
+    {id: 2, name: "cerdo" , price: 1000, category: "carne" , stock :100 , description :"bondiola" ,img:"https://www.stock.com.py/images/thumbs/0187242.jpeg"},
+    {id: 3, name: "pollo" , price: 1000, category: "carne" , stock :100 , description :"pechuga" ,img:"https://www.ariztiaatucasa.cl/media/catalog/product/cache/3aa1df24426dbabd72ab974cf5ed84b3/2/9/29496bf-01.jpg"},
+]
+
+export const getProducts =() => {
+    return new Promise (resolve => {
+        setTimeout(()=>{
+            resolve(products)
+        },2000 )
+    })
+}
